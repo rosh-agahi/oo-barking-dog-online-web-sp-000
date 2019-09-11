617f8fbdf861dccab4547cedd9b9ff541b112b61
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-11 17:24:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,16 @@
 class Dog 
   
   def name=(dog_name)
-    @name
+    @this_dogs_name = dog_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
   
 end
 
